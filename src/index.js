b8377c79ea5e75aed881e61f3edce86ea2174bfe
Jordan Kohl <kohl.jordan@gmail.com>
2016-06-08 15:00:47 -0700

    fix(provide): pass props down to the wrapped component
    Closes #1
    
    Closes #1

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default (store) => {
   return (Component) =>
-    (props) => React.createElement(Component, { ...store, ...props })
+    (props) => React.createElement(Component, Object.assign({}, store, props))
 }
 
